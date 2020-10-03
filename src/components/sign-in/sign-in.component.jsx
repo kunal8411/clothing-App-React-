9a4425c25d295219c4b8edca35c1bcf34932a42b
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-03 18:07:34 +0530

    Button COmponent(82)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 class SignIn extends React.Component{
     constructor(props){
         super(props);
@@ -45,10 +46,10 @@ class SignIn extends React.Component{
                      value={this.state.password} 
                      name="password" 
                      required 
-                    />
+                    />  
                     
-                    <input type="submit" value="Submit"/>
-                </form>
+                    <CustomButton type="submit" >Sign In</CustomButton>
+                </form> 
             </div>
         )
     }
