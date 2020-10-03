013d5dbc04bd3ba4a8c9b8064cf39148ed52f7f4
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-03 13:48:56 +0530

    Header Component(77)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
 import { Route}  from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component'
-// const HatsPage=()=>(
-//   <div>
-//     <h1>Hats Page</h1>
-//   </div>
-// );
+import ShopPage from './pages/shop/shop.component';
+import Header from '../src/components/header/header.component'
 
 function App() {
   return (
     <div >
+      <Header/ >
       <switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+       
       </switch>
     </div>
   );
