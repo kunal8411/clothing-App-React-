02a1363a38c706d7927d722a55e44a330ef6e853
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-03 11:13:16 +0530

    shp Page(75)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
 import { Route}  from 'react-router-dom';
-
-const HatsPage=()=>(
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component'
+// const HatsPage=()=>(
+//   <div>
+//     <h1>Hats Page</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div >
       <switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </switch>
     </div>
   );
