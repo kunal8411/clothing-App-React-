744b8aa0f0efa288e2b900776715ad5506f78631
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-05 18:30:02 +0530

    login with email and passswor and store the session(99)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component';
-import  {signInWithGoogle} from '../../firebase/firebase.utils'
+import  {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 class SignIn extends React.Component{
     constructor(props){
         super(props);
@@ -12,10 +12,18 @@ class SignIn extends React.Component{
         password:''
         }     
     }
-    handleSubmit=(event)=>{
+    handleSubmit=async(event)=>{
         event.preventDefault();
+        const {email, password}= this.state;
 
-        this.setState({email:'', password:''})
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''})
+        }catch(error){
+            console.log("error",error)
+        }
+
+        
     }
     handleChange=(event)=>{
         // console.log(event.target)
