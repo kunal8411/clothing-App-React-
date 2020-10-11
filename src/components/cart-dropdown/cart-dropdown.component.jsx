3b28eef19b276fb0d2ef75d800d409ff1a7435b9
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-11 17:41:34 +0530

    119- push items into dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component'
+import {connect} from 'react-redux'
 import './cart-dropdown.styles.scss'
-
-const CartDropdown =()=>(
+import CartItem from '../cart-item/cart-item.component'
+const CartDropdown =({cartItems})=>(
     <div className='cart-dropdown'>
-        <div className='cart-items'></div>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem=>(
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                ))
+            }
+        </div>
         <CustomButton >CHECKOUT</CustomButton>
     </div>
 )
 
+const mapStateToProps =({cart:{cartItems}})=>({
+    cartItems
+})
 
-export default CartDropdown;
+
+export default connect(mapStateToProps)(CartDropdown);
