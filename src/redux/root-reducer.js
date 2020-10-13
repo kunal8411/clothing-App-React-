0caa8bdd89e225e30626a780ded5da04faeaecda
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-13 17:59:10 +0530

    135-set home page section into redux store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,7 +4,7 @@ import  {combineReducers} from 'redux';
 import {persistReducer} from 'redux-persist';
 import userReducer from './user/user.reducer';
 import  cartReducer from './cart/cart.reducer';
-
+import direcoryReducer from './directory/directory.reducer'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -15,7 +15,9 @@ const persistConfig = {
 
 const rootReducer =combineReducers({
     user:userReducer,
-    cart:cartReducer  
+    cart:cartReducer,
+    //this directory contains all the information which we are rendering on home page
+    directory:direcoryReducer  
 })
 
 
