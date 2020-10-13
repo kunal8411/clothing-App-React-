3c9261d4c444d154896b120aff8af629d6fa79eb
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-13 16:26:46 +0530

    134-redux persist, add cart data into local storage

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
-
+import {persistGate} from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
-import store from './redux/store'
+import {store, persistor} from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
   
     <BrowserRouter >
-    <App />
+    <persistGate persistor={persistor}>
+      <App />
+    </persistGate>
+    
     </BrowserRouter>
   </Provider>,
   
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 //actual base reducer object contains whole state
 
 import  {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
 import userReducer from './user/user.reducer';
-import  cartReducer from './cart/cart.reducer'
-export default combineReducers({
-    user:userReducer  ,
+import  cartReducer from './cart/cart.reducer';
+
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key:'root', 
+    storage, 
+    whiteist:['cart']
+}
+
+const rootReducer =combineReducers({
+    user:userReducer,
     cart:cartReducer  
-});
+})
+
+
+export default persistReducer(persistConfig, rootReducer)
